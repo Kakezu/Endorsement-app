7c0bab2aadc25d0489efb83d4481d09422e26dbe
Kakezu <kaius.vanajas@student.laurea.fi>
2023-09-07 11:48:44 +0300

    added javascript to append list items from input

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const inputEl = document.getElementById("input-el")
 const publishBtn = document.getElementById("publish-btn")
-
+const endorsementList = document.getElementById("endorsement-list")
 
 publishBtn.addEventListener("click", function() {
-    console.log(inputEl.value)
+    appendInputToEndorsementList(inputEl.value)
+
+    clearInputText()
 })
+
+function clearInputText() {
+    inputEl.value = ""
+}
+
+function appendInputToEndorsementList(input) {
+    let newEl = document.createElement("li")
+
+    newEl.textContent = input
+    if (input === "") {
+        console.log("Need to type something!")
+    } else {
+        endorsementList.append(newEl)
+    }
+}
