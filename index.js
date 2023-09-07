0fccc5f50aa1a2d685ce72372f30a4f027181734
Kakezu <kaius.vanajas@student.laurea.fi>
2023-09-07 11:35:23 +0300

    added skeleton with HTML/CSS

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-el")
+const publishBtn = document.getElementById("publish-btn")
+
+
+publishBtn.addEventListener("click", function() {
+    console.log(inputEl.value)
+})
